d511a82c5cd0b10169725b5b2daabb1114c38cac
João Pedro Bagnara Boeing <106355107+DevJoaoPBB@users.noreply.github.com>
2025-04-03 17:35:34 -0300

    Update loginView.jsx

--- src/Views/Login/loginView.jsx
+++ src/Views/Login/loginView.jsx
@@ -36,7 +36,7 @@ export default function Login() {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:7373/login", {
+      const response = await fetch("https://chatbotinteligente-x5rt.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, senha }),
